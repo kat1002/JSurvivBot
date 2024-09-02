f644b641b1c1ff5649d7a731e062f0d47971536c
kat1002 <nguyenphuc10022004@gmail.com>
2024-09-02 23:41:32 +0700

    import JSurviv SDK

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import io.socket.emitter.Emitter;
+import jsclub.codefest2024.sdk.model.GameMap;
+import jsclub.codefest2024.sdk.Hero;
+
+import java.io.IOException;
+
+public class Main {
+    private static final String SERVER_URL = "https://cf-server.jsclub.dev";
+    private static final String GAME_ID = "168470";
+    private static final String PLAYER_NAME = "MikuQueen";
+
+
+    public static void main(String[] args) throws IOException {
+        Hero hero = new Hero(GAME_ID, PLAYER_NAME);
+
+        Emitter.Listener onMapUpdate = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+
+                GameMap gameMap = hero.getGameMap();
+                gameMap.updateOnUpdateMap(args[0]);
+
+
+            }
+        };
+
+        hero.setOnMapUpdate(onMapUpdate);
+        hero.start(SERVER_URL);
+    }
+}
